7f9c76215ecdff1ac5c1914f30419ca4bbca240e
Fumikazu Hoshi <fumikazu.hoshi@jp.ricoh.com>
2023-09-30 22:11:46 +0900

    update

--- README.txt
+++ README.txt
@@ -1,8 +1,11 @@
 Notes リンクを Teams 形式に変換するツール
 
-noteslink3.exe
+NotesLink2SPO（Notes://ZARG11/4925824B001210A2）の単体アプリ版です
 
+noteslink3.exe【推奨】
+リダイレクト先：http://rgrsc01.nws.ricoh.co.jp/common/NUReSPO.nsf/
+    
 ＜使い方＞
-(1) noteslink3.exe をダウンロードして適当な場所に保存してください。
+(1) noteslink3.exe / noteslink3_spo.exe をダウンロードして適当な場所に保存してください。
 (2) Notes の文書リンクをクリップボードにコピーした後、noteslink3.exe を実行してください。
 (3) 変換後のリンクがクリップボードにコピーされますので、Teams に貼り付けてください。
--- noteslink3.py
+++ noteslink3.py
@@ -12,14 +12,20 @@ Usage:
     URI 変換後の文字列がクリップボードにセットされます。
 """
 
+# 参考）Redmineチケットのリンク記法
+# "文書名":Notes://ZARG11/49257F99001E6010/492562B700343D44492562980005DADD/25D322737C66833A492584BF002D84C2
+
 import os
 import sys
 import re
-
 import pyperclip as clip
 
-REDIRECT = 'https://rfgricoh.sharepoint.com/sites/rfgportal/SitePages/redirect.aspx?'
-# REDIRECT = 'http://10.63.100.25/notes/?uri='
+TYPE = 'SPO'  # SPO or RFG
+
+if TYPE == 'SPO':
+    REDIRECT = 'http://rgrsc01.nws.ricoh.co.jp/common/NUReSPO.nsf/'
+else:
+    REDIRECT = 'https://rfgricoh.sharepoint.com/sites/rfgportal/SitePages/redirect.aspx?'
 
 
 def main():
@@ -51,11 +57,18 @@ def main():
         m = re.search(r'<NOTE OF(\w+):(\w+)-ON(\w+):(\w+)>', text)
         if m:
             pathlist.append(''.join(m.groups()))
+
         # URI
-        uri = 'Notes://' + '/'.join(pathlist)
+        if TYPE == 'SPO':
+            # SPO：NotesLink2SPO（Notes://ZARG11/4925824B001210A2）と同じ機能
+            uri = 'NotesURL?open&//' + '&'.join(pathlist)
+        else:
+            # RFG：リコーグループポータルのリダイレクト機能
+            uri = 'Notes://' + '/'.join(pathlist)
+
         # Markdown
-        # uri = '[{0}]({1})'.format(title, REDIRECT + uri)  # Teams 用
-        uri = '[[{1}|{0}]]'.format(title, uri)  # DokuWiki 用
+        uri = '[{0}]({1})'.format(title, REDIRECT + uri)  # Teams 用
+        # uri = '[[{1}|{0}]]'.format(title, uri)  # DokuWiki 用
     else:
         uri = text
 
@@ -66,14 +79,18 @@ def main():
         uri = '[{0}]({1})'.format(title, REDIRECT + uri)  # Teams 用
         # uri = '[[{1}|{0}]]'.format(title, uri)  # DokuWiki 用
 
+    # 英単語の前後にスペースを挿入する
+    # uri = re.sub(r'([\s]*[\a-zA-Z_0-9]+[\s]*)', r' \1 ', uri)
+
     # 変換後の URI をクリップボードにセットする
     if uri:
+        # 行末尾のスペース削除
+        # uri = '\r\n'.join(line.rstrip() for line in uri.split('\n'))
         print('\nクリップボードにコピーしました。')
         print('\n' + uri)
         clip.copy(uri)
 
-
 if __name__ == '__main__':
     print(os.path.basename(sys.argv[0]))
     main()
-    input('\nPress Enter to exit.')
+    # input('\nPress Enter to exit.')
